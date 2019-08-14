02b634df9e3e110ffe066678ac55084d8cd34c2a
Matt Puchyr <matt.puchyr@gmail.com>
2019-08-14 22:55:30 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
 # Your Code Here
+def map(source_array)
+  counter = 0
+  new = []
+  while counter < source_array.length do
+    new.push(yield(source_array[counter]))
+    counter += 1
+  end
+  new
+end
+
+def reduce(source_array, starting_point = nil)
+  if starting_point
+    s = starting_point
+    counter = 0
+  else
+    s = source_array[0]
+    counter = 1
+  end
+  while counter < source_array.length do
+    s = yield(s, source_array[counter])
+    counter += 1
+  end
+  s
+end
+
