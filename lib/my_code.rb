91178223e333620e34a143392b4302254e3d148e
Matt Puchyr <matt.puchyr@gmail.com>
2019-08-14 22:02:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,17 @@
 # Your Code Here
+def map(source_array)
+  counter = 0
+  new = []
+  while counter < source_array.length do
+    new.push(yield(source_array[counter]))
+    counter += 1
+  end
+  new
+end
+
+def reduce(source_array, starting_point = 0)
+  counter = 0
+  while counter < source_array.length do
+  end
+end
+
